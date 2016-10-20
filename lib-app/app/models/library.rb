81a286aa5c96cc5087e7a6bfd5d93e4f7f8ed47c
rhamill1 <rhamill11@gmail.com>
2016-10-20 13:26:56 -0700

    uncommited updates from yesterday

--- lib-app/app/models/library.rb
+++ lib-app/app/models/library.rb
@@ -1,2 +1,4 @@
 class Library < ApplicationRecord
+  has_many :library_users, dependent: :destroy
+  has_many :libraries, through: :library_users
 end
--- lib-app/app/models/user.rb
+++ lib-app/app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+
+  has_many :library_users, dependent: :destroy
+  has_many :libraries, through: :library_users
+
   has_secure_password
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user ? @user.authenticate(params[:password]) : false
   end
+
 end
